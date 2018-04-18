77f670223f49411c436e71d2ff9fc469ec9fb9ad
Muhammad Ibrahim <muhammed.ibrahim011149@gmail.com>
2018-04-18 10:23:38 +0200

    GET todo/:id and its test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('ID is not valid!')
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send('Todo was not found!');
+    }
+    res.send(todo)
+  }, (e) => {
+    res.status(404).send('Todo was not found')
   });
 });
 
